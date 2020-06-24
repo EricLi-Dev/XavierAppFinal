3489b4e24b48bcc5f2ced354b6060e3765d2b65c
TJ Ledwith <makertech81@gmail.com>
2020-06-24 18:08:00 -0400

    Previews Updated

--- XavierApp/XavierApp/OnboardingView.swift
+++ XavierApp/XavierApp/OnboardingView.swift
@@ -4,5 +4,16 @@
 //
 //  Created by Eric Li on 6/24/20.
 //
+import SwiftUI
 
-import Foundation
+struct OnboardingView: View {
+    var body: some View {
+        Text("Hello, yid!").padding()
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
