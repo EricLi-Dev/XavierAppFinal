b2cf66c531ff8145e7d2c35e3835a0c06cad1aee
TJ Ledwith <makertech81@gmail.com>
2020-06-24 17:00:42 -0400

    Created project files.

---
+++ XavierApp/XavierApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  XavierApp
+//
+//  Created by Thomas Ledwith on 6/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XavierApp/XavierApp/XavierAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XavierAppApp.swift
+//  XavierApp
+//
+//  Created by Thomas Ledwith on 6/24/20.
+//
+
+import SwiftUI
+
+@main
+struct XavierAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
